d94dda19d6c4765c1e2a6d7bc00c2b5848213cac
Chandan10 <chandangirish95@gmail.com>
2026-04-13 22:03:34 +0530

    Added api

--- frontend/lib/core/constants/api_constants.dart
+++ frontend/lib/core/constants/api_constants.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/foundation.dart';
 
 class ApiConstants {
+  static const String productionBaseUrl =
+      'https://rokon-al-shiuokh.onrender.com/api';
+  static const String webDevelopmentBaseUrl = 'http://localhost:5000/api';
+  static const String emulatorDevelopmentBaseUrl = 'http://10.0.2.2:5000/api';
+
   static String get baseUrl {
     const configuredBaseUrl = String.fromEnvironment('API_BASE_URL');
     if (configuredBaseUrl.isNotEmpty) {
       return configuredBaseUrl;
     }
 
-    if (kIsWeb) {
-      return 'http://localhost:5000/api';
+    if (kReleaseMode) {
+      return productionBaseUrl;
     }
 
-    return 'http://10.0.2.2:5000/api';
+    if (kIsWeb) {
+      return webDevelopmentBaseUrl;
+    }
+
+    return emulatorDevelopmentBaseUrl;
   }
 }
